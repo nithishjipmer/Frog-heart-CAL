2d31274e22bcd4e51c666a240b803f5bbb0f4445
Nithish Kumar Pasupathi <100558366+nithishjipmer@users.noreply.github.com>
2022-10-10 01:03:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ var mqls = [
 function mediaqueryresponse(mql) {
   if (mqls[0].matches) {
     // ipad query matched
-    shown = 100;
+    shown = 580;
   }
   if (mqls[1].matches) {
     // ipad pro query matched
