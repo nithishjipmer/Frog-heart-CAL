d59bfa2d93460d8b95e315ae6b8851b30a7ffeec
Nithish Kumar Pasupathi <100558366+nithishjipmer@users.noreply.github.com>
2022-10-10 11:58:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var shown = 750;
+var shown = 550;
 var table;
 table = document.getElementById("drugtable");
 var n = 0
@@ -15,44 +15,7 @@ correctAns = correctAns = table.options[rdm].text;
 
 
 // responsive
-var mqls = [
-  window.matchMedia(
-    "(min-device-width: 1024px) and (max-device-width: 1024px) and (orientation: landscape) and (-webkit-min-device-pixel-ratio: 2)"
-  ),
-  window.matchMedia(
-    "(min-device-width: 1112px) and (max-device-width: 1112px) and (orientation: landscape) and (-webkit-min-device-pixel-ratio: 2)"
-  ),
-  window.matchMedia(
-    "(min-device-width: 1366px) and (max-device-width: 1366px) and (orientation: landscape) and (-webkit-min-device-pixel-ratio: 2)"
-  ),
-  window.matchMedia(
-    "(min-device-width: 1180px) and (max-device-width: 1180px) and (orientation: landscape) and (-webkit-min-device-pixel-ratio: 2)"
-  ),
-];
 
-function mediaqueryresponse(mql) {
-  if (mqls[0].matches) {
-    // ipad query matched
-    shown = 580;
-  }
-  if (mqls[1].matches) {
-    // ipad pro query matched
-    shown = 580;
-  }
-  if (mqls[2].matches) {
-    // for ipad pro 12.9' query matched
-    shown = 750;
-  }
-  if (mqls[1].matches) {
-    // ipad air
-    shown = 580;
-  }
-}
-
-for (var i = 0; i < mqls.length; i++) {
-  mediaqueryresponse(mqls[i]); // call listener function explicitly at run time
-  mqls[i].addListener(mediaqueryresponse); // attach listener function to listen in on state changes
-}
 
 
 
